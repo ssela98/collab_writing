4108e2547ec2c1eba827bade093c077e3035c721
ssela98 <tirnavanalessandra@gmail.com>
2022-11-15 21:36:31 +0100

    update config envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  # Set actionmailer host to localhost
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :test
 
