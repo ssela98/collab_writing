e8af9655b122431bb15c9fe5d2f8bbc56051d4f9
ssela98 <tirnavanalessandra@gmail.com>
2022-11-17 01:38:14 +0100

    rerun migration, update schema

--- db/migrate/20221116222248_add_username_to_users.rb
+++ db/migrate/20221116222248_add_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddUsernameToUsers < ActiveRecord::Migration[7.0]
   def up
-    add_column :users, :username, :string, if_not_exists: true
+    add_column :users, :username, :string, null: false, if_not_exists: true
     add_index :users, :username, unique: true, if_not_exists: true
   end
 
