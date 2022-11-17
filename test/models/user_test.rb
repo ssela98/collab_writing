d2701f59a2083a1f2ad62794e523c987c2841058
ssela98 <tirnavanalessandra@gmail.com>
2022-11-17 21:01:05 +0100

    improve user unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,20 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'creating user with username works' do
-    user = User.create(email: 'user@email.com', username: 'user', password: 'password')
+    user = User.create(email: Faker::Internet.email, username: Faker::Internet.username, password: Faker::Internet.password)
 
     assert user.valid?
   end
 
   test 'creating user without username should not work' do
-    user = User.create(email: 'user@email.com', password: 'password')
+    user = User.create(email: Faker::Internet.email, password: Faker::Internet.password)
 
     assert_not user.valid?
     assert_equal ["Username can't be blank"], user.errors.full_messages
   end
 
   test 'creating user with too long username should not work' do
-    user = User.create(email: 'user@email.com', username: 'a' * 25, password: 'password')
+    user = User.create(email: Faker::Internet.email, username: ('a'..'z').to_a.sample(25).join, password: Faker::Internet.password)
 
     assert_not user.valid?
     assert_equal ['Username is too long (maximum is 24 characters)'], user.errors.full_messages
