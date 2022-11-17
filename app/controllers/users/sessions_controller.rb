32fc8ceda13bc4e29c536758c88f00446e9f3587
ssela98 <tirnavanalessandra@gmail.com>
2022-11-17 21:49:11 +0100

    redirect to home page after signing in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,9 @@ class Users::SessionsController < Devise::SessionsController
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
   end
+
+  # The path used after sign in.
+  def after_sign_in_path(resource)
+    root_path
+  end
 end
