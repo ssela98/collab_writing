0da170ff7cba03ee88d0fc9019e93730a602a52b
ssela98 <tirnavanalessandra@gmail.com>
2022-11-18 12:11:46 +0100

    add more tests to sessions_controller and improve the old ones

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -10,6 +10,7 @@ module Users
       post user_session_path(user: { username: user.username, password: user.password })
 
       assert_equal user, @controller.current_user
+      assert_redirected_to root_path
     end
 
     test 'create without username should fail' do
@@ -17,6 +18,25 @@ module Users
 
       post user_session_path(user: { password: user.password })
 
+      # TODO: assert flash messages when they're introduced
+      assert_nil @controller.current_user
+    end
+
+    test 'create without password should fail' do
+      user = create(:user)
+
+      post user_session_path(user: { username: user.username })
+
+      # TODO: assert flash messages when they're introduced
+      assert_nil @controller.current_user
+    end
+
+    test 'create with non-existing username should fail' do
+      user = create(:user)
+
+      post user_session_path(user: { username: ('a'..'z').to_a.sample(25).join, password: user.password })
+
+      # TODO: assert flash messages when they're introduced
       assert_nil @controller.current_user
     end
   end
