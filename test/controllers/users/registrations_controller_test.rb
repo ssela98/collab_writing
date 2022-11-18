9f0343d852c57f7bb4e22f026490f594dadbe903
ssela98 <tirnavanalessandra@gmail.com>
2022-11-18 12:09:57 +0100

    add more tests to registration_controller and improve the old ones

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -5,15 +5,16 @@ require 'test_helper'
 module Users
   class RegistrationsControllerTest < ActionDispatch::IntegrationTest
     # TODO: uncomment when you reenable user :registerable
-    # test "create should send mail and redirect to login" do
+    # test 'create should send email and redirect to login' do
     #   email = Faker::Internet.email
     #   password = Faker::Internet.password
-    #   sign_up_params = { email: email, username: Faker::Internet.username, password: password, password_confirmation: password }
+    #   sign_up_params = { email:, username: Faker::Internet.username, password:,
+    #     password_confirmation: password }
 
     #   post user_registration_path(user: sign_up_params)
     #   assert_redirected_to new_user_session_path
 
-    #   user = User.find_by(email: email)
+    #   user = User.find_by(email:)
     #   mail = ActionMailer::Base.deliveries.last
 
     #   assert_equal [sign_up_params[:email]], mail.to
@@ -22,16 +23,57 @@ module Users
     #   assert mail.decoded.include?(user_confirmation_path(confirmation_token: user.confirmation_token))
     # end
 
-    # test "create without username should fail" do
-    #   email = Faker::Internet.email
+    # test 'create without email should fail' do
+    #   username = Faker::Internet.username
     #   password = Faker::Internet.password
-    #   sign_up_params = { email: email, password: password, password_confirmation: password }
+    #   sign_up_params = { username: Faker::Internet.username, password:,
+    #     password_confirmation: password }
 
     #   assert_difference 'User.count', 0 do
     #     post user_registration_path(user: sign_up_params)
     #   end
 
-    #   assert_not User.find_by(email: email)
+    #   # assert flash messages when they're introduced
+    #   assert_not User.find_by(username:)
+    # end
+
+    # test 'create without username should fail' do
+    #   email = Faker::Internet.email
+    #   password = Faker::Internet.password
+    #   sign_up_params = { email:, password:, password_confirmation: password }
+
+    #   assert_difference 'User.count', 0 do
+    #     post user_registration_path(user: sign_up_params)
+    #   end
+
+    #   # assert flash messages when they're introduced
+    #   assert_not User.find_by(email:)
+    # end
+
+    # test 'create without password should fail' do
+    #   email = Faker::Internet.email
+    #   sign_up_params = { email:, username: Faker::Internet.username,
+    #     password_confirmation: Faker::Internet.password }
+
+    #   assert_difference 'User.count', 0 do
+    #     post user_registration_path(user: sign_up_params)
+    #   end
+
+    #   # assert flash messages when they're introduced
+    #   assert_not User.find_by(email:)
+    # end
+
+    # test 'create with unmatching password confirmation should fail' do
+    #   email = Faker::Internet.email
+    #   sign_up_params = { email:, username: Faker::Internet.username,
+    #     password: Faker::Internet.password, password_confirmation: Faker::Internet.password }
+
+    #   assert_difference 'User.count', 0 do
+    #     post user_registration_path(user: sign_up_params)
+    #   end
+
+    #   # assert flash messages when they're introduced
+    #   assert_not User.find_by(email:)
     # end
   end
 end
