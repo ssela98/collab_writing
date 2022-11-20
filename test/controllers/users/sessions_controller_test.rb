55e9f53252987dd853284d68eb2b07bb717d3f29
ssela98 <tirnavanalessandra@gmail.com>
2022-11-20 19:00:41 +0100

    add flash asserts to sessions_controller_test

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -4,6 +4,8 @@ require 'test_helper'
 
 module Users
   class SessionsControllerTest < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
+
     test 'should create' do
       user = create(:user)
 
@@ -11,6 +13,27 @@ module Users
 
       assert_equal user, @controller.current_user
       assert_redirected_to root_path
+      assert_equal I18n.t('devise.sessions.signed_in'), flash[:notice]
+    end
+
+    test 'should not get new if already signed in' do
+      user = create(:user)
+      sign_in user
+
+      get new_user_session_path
+
+      assert_redirected_to root_path
+      assert_equal I18n.t('devise.failure.already_authenticated'), flash[:alert]
+    end
+
+    test 'should not create if already signed in' do
+      user = create(:user)
+      sign_in user
+
+      post user_session_path(user: { username: user.username, password: user.password })
+
+      assert_redirected_to root_path
+      assert_equal I18n.t('devise.failure.already_authenticated'), flash[:alert]
     end
 
     test 'should not create without username' do
