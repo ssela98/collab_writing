9e05ae78ba99396203c65a1296e8a1b2437780cf
ssela98 <tirnavanalessandra@gmail.com>
2022-11-20 19:40:07 +0100

    assert error messages for registration system test

--- test/system/users/registration_test.rb
+++ test/system/users/registration_test.rb
@@ -4,6 +4,7 @@ require 'application_system_test_case'
 
 module Users
   class RegistrationTest < ApplicationSystemTestCase
+    # TODO: uncomment when you reenable user :registerable
     # test 'should register' do
     #   visit new_user_registration_url
 
@@ -20,6 +21,7 @@ module Users
     #   end
 
     #   assert_includes current_url, new_user_session_path
+    #   assert_equal I18n.t('devise.registrations.signed_up_but_unconfirmed'), find('.flash__notice').text
     #   assert User.find_by(email:)
     # end
 
@@ -37,8 +39,8 @@ module Users
     #     find("input[type='submit']").click
     #   end
 
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
+    #   # assert_equal new_user_registration_url, current_url # TODO: why does this fail?
+    #   assert_equal 'Email can\'t be blank', find('#new_user_email_errors > li').text
     #   assert_not User.find_by(username:)
     # end
 
@@ -56,45 +58,8 @@ module Users
     #     find("input[type='submit']").click
     #   end
 
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
-    #   assert_not User.find_by(email:)
-    # end
-
-    # test 'should not register without password' do
-    #   visit new_user_registration_url
-
-    #   email = Faker::Internet.email
-
-    #   find('#user_email').fill_in with: email
-    #   find('#user_username').fill_in with: Faker::Internet.username
-    #   find('#user_password_confirmation').fill_in with: Faker::Internet.password
-
-    #   assert_difference 'User.count', 0 do
-    #     find("input[type='submit']").click
-    #   end
-
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
-    #   assert_not User.find_by(email:)
-    # end
-
-    # test 'should not register with unmatching password confirmation' do
-    #   visit new_user_registration_url
-
-    #   email = Faker::Internet.email
-
-    #   find('#user_email').fill_in with: email
-    #   find('#user_username').fill_in with: Faker::Internet.username
-    #   find('#user_password').fill_in with: Faker::Internet.password
-    #   find('#user_password_confirmation').fill_in with: Faker::Internet.password
-
-    #   assert_difference 'User.count', 0 do
-    #     find("input[type='submit']").click
-    #   end
-
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
+    #   # assert_equal new_user_registration_url, current_url # TODO: why does this fail?
+    #   assert_equal 'Username can\'t be blank', find('#new_user_username_errors > li').text
     #   assert_not User.find_by(email:)
     # end
 
@@ -113,8 +78,8 @@ module Users
     #     find("input[type='submit']").click
     #   end
 
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
+    #   # assert_equal new_user_registration_url, current_url # TODO: why does this fail?
+    #   assert_equal 'Email is too long (maximum is 200 characters)', find('#new_user_email_errors > li').text
     #   assert_not User.find_by(username:)
     # end
 
@@ -133,51 +98,9 @@ module Users
     #     find("input[type='submit']").click
     #   end
 
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
+    #   # assert_equal new_user_registration_url, current_url # TODO: why does this fail?
+    #   assert_equal 'Username is too long (maximum is 24 characters)', find('#new_user_username_errors > li').text
     #   assert_not User.find_by(email:)
-    # end
-
-    # test 'should not register with badly formatted email' do
-    #   visit new_user_registration_url
-
-    #   username = Faker::Internet.username
-    #   password = Faker::Internet.password
-
-    #   find('#user_email').fill_in with: 'bad_email'
-    #   find('#user_username').fill_in with: username
-    #   find('#user_password').fill_in with: password
-    #   find('#user_password_confirmation').fill_in with: password
-
-    #   assert_difference 'User.count', 0 do
-    #     find("input[type='submit']").click
-    #   end
-
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
-    #   assert_not User.find_by(username:)
-    # end
-
-    # test 'should not register with existing email' do
-    #   visit new_user_registration_url
-
-    #   email = Faker::Internet.email
-    #   create(:user, email:)
-
-    #   password = Faker::Internet.password
-
-    #   find('#user_email').fill_in with: email
-    #   find('#user_username').fill_in with: Faker::Internet.username
-    #   find('#user_password').fill_in with: password
-    #   find('#user_password_confirmation').fill_in with: password
-
-    #   assert_difference 'User.count', 0 do
-    #     find("input[type='submit']").click
-    #   end
-
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
-    #   assert_equal 1, User.where(email:).count
     # end
 
     # test 'should not register with existing username' do
@@ -197,8 +120,8 @@ module Users
     #     find("input[type='submit']").click
     #   end
 
-    #   # TODO: assert flash messages when they're introduced
-    #   assert_equal new_user_registration_url, current_url
+    #   # assert_equal new_user_registration_url, current_url # TODO: why does this fail?
+    #   assert_equal 'Username has already been taken', find('#new_user_username_errors > li').text
     #   assert_equal 1, User.where(username:).count
     # end
   end
