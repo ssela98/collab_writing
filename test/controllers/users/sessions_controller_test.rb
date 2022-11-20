a4e755ffa10039142f93cd65816b937538495a34
ssela98 <tirnavanalessandra@gmail.com>
2022-11-20 19:39:19 +0100

    remove more sessions_controller tests

--- test/controllers/users/sessions_controller_test.rb
+++ test/controllers/users/sessions_controller_test.rb
@@ -16,48 +16,10 @@ module Users
       assert_equal I18n.t('devise.sessions.signed_in'), flash[:notice]
     end
 
-    test 'should not get new if already signed in' do
-      user = create(:user)
-      sign_in user
-
-      get new_user_session_path
-
-      assert_redirected_to root_path
-      assert_equal I18n.t('devise.failure.already_authenticated'), flash[:alert]
-    end
-
-    test 'should not create if already signed in' do
-      user = create(:user)
-      sign_in user
-
-      post user_session_path(user: { username: user.username, password: user.password })
-
-      assert_redirected_to root_path
-      assert_equal I18n.t('devise.failure.already_authenticated'), flash[:alert]
-    end
-
     test 'should not create without username' do
       user = create(:user)
 
       post user_session_path(user: { password: user.password })
-
-      assert_equal I18n.t('devise.failure.invalid', authentication_keys: 'Username'), flash[:alert]
-      assert_nil @controller.current_user
-    end
-
-    test 'should not create without password' do
-      user = create(:user)
-
-      post user_session_path(user: { username: user.username })
-
-      assert_equal I18n.t('devise.failure.invalid', authentication_keys: 'Username'), flash[:alert]
-      assert_nil @controller.current_user
-    end
-
-    test 'should not create with unmatching password' do
-      user = create(:user)
-
-      post user_session_path(user: { username: user.username, password: Faker::Internet.password })
 
       assert_equal I18n.t('devise.failure.invalid', authentication_keys: 'Username'), flash[:alert]
       assert_nil @controller.current_user
