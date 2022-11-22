66c9f9c1c311ad24ed4f5dcc89cf69ec4e2675a1
ssela98 <tirnavanalessandra@gmail.com>
2022-11-22 20:45:08 +0100

    add forbidden_unless_creator to comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_comment
+  before_action :forbidden_unless_creator, only: %i[edit update destroy]
 
   def show; end
 
@@ -35,8 +36,20 @@ class CommentsController < ApplicationController
 
   private
 
+  # Do not allow edit, update or destroy changes if the logged in user
+  # is not the creator of the story
+  def forbidden_unless_creator
+    return if current_user == @comment.user
+
+    flash.now[:alert] = I18n.t('stories.alerts.not_the_creator')
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.prepend('flash', partial: 'shared/flash') }
+      format.html { redirect_to @comment.commentable }
+    end 
+  end
+
   def set_comment
-    @comment = current_user.comments.find(params[:id])
+    @comment = Comment.find_by(id: params[:id])
   end
 
   def comment_params
