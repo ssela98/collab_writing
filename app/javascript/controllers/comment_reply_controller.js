2e1820892591b8c6ce6bf160029f209b625bf6ce
ssela98 <tirnavanalessandra@gmail.com>
2022-11-23 00:14:13 +0100

    remove console.log from comment_reply stimulus controller

--- app/javascript/controllers/comment_reply_controller.js
+++ app/javascript/controllers/comment_reply_controller.js
@@ -4,10 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ 'form' ]
 
-  connect() {
-    console.log('hello')
-  }
-
   toggle(event) {
     event.preventDefault()
     this.formTarget.classList.toggle('d-none')
