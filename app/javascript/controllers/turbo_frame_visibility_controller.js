7caef7419f1f07364f8c916566baaaa6fc28bc1c
ssela98 <tirnavanalessandra@gmail.com>
2022-11-23 00:16:35 +0100

    add turbo_frame_visiblity stimulus controller

---
+++ app/javascript/controllers/turbo_frame_visibility_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="turbo-frame-visibility-controller"
+export default class extends Controller {
+  static targets = [ 'hideableTurboFrame' ]
+
+  connect() {
+    let currentUserUsername = document.querySelector("meta[name='current-user-username']").content
+    if (!currentUserUsername) return
+
+    if (this.hideableTurboFrameTarget.dataset.usernameValue != currentUserUsername)
+      this.hideableTurboFrameTarget.classList.toggle('d-none')
+  }
+}
