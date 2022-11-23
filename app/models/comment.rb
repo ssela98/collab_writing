c57243e21162a27dd4000b4046ae424983ec420f
ssela98 <tirnavanalessandra@gmail.com>
2022-11-23 15:32:45 +0100

    break scope into multi lines

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -22,8 +22,13 @@ class Comment < ApplicationRecord
 
   has_rich_text :content
   scope :where_content_like, lambda { |content|
-                               joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = comments.id AND action_text_rich_texts.record_type = 'Comment'").where('action_text_rich_texts.body LIKE ?', "%#{content}%")
-                             }
+    join_sql_query = "INNER JOIN action_text_rich_texts
+      ON action_text_rich_texts.record_id = comments.id
+      AND action_text_rich_texts.record_type = 'Comment'"
+    where_sql_query = 'action_text_rich_texts.body LIKE ?', "%#{content}%"
+
+    joins(join_sql_query).where(where_sql_query)
+  }
 
   validates :content, presence: true
 
