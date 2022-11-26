dcf89acb027cd8939fee0e49865b023e4ff71503
ssela98 <tirnavanalessandra@gmail.com>
2022-11-26 14:42:27 +0100

    clean up pins partials, routes and controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,52 +1,32 @@
 # frozen_string_literal: true
 
 class PinsController < ApplicationController
-  before_action :authenticate_user!, except: :show
-  before_action :set_pin, only: %i[show edit update destroy]
-  before_action :set_story, only: %i[create]
-
-  # GET /pins or /pins.json
-  def index
-    @pins = Pin.all
-  end
-
-  # GET /pins/1 or /pins/1.json
-  def show; end
-
-  # GET /pins/new
-  def new
-    @pin = Pin.new
-  end
-
-  # GET /pins/1/edit
-  def edit; end
+  before_action :authenticate_user!
+  before_action :set_pin, except: %i[create]
 
   # POST /pins or /pins.json
   def create
     @pin = Pin.new(pin_params)
-    @pin.save # TODO: split into error handling
+
+    if @pin.save
+      flash.now[:notice] = I18n.t('pins.notices.successfully_created')
+    else
+      flash.now[:alert] = I18n.t('pins.errors.failed_to_create')
+    end
   end
 
-  # PATCH/PUT /pins/1 or /pins/1.json
+  # PATCH/PUT pins/1
   def update
-    respond_to do |format|
-      if @pin.update(pin_params)
-        format.html { redirect_to @pin.story, notice: I18n.t('pins.notices.successfully_updated') }
-        format.json { render :show, status: :ok, location: @pin }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /pins/1 or /pins/1.json
   def destroy
     @pin.destroy
 
-    respond_to do |format|
-      format.html { redirect_to @pin.story, notice: I18n.t('pins.notices.successfully_destroyed') }
-      format.json { head :no_content }
+    if @pin.destroyed?
+      flash.now[:notice] = I18n.t('pins.notices.successfully_destroyed')
+    else
+      flash.now[:alert] = I18n.t('pins.errors.failed_to_destroy')
     end
   end
 
@@ -55,10 +35,6 @@ class PinsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_pin
     @pin = Pin.find(params[:id])
-  end
-
-  def set_story
-    @story = Story.find(params[:pin][:story_id])
   end
 
   # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :pins
+  resources :pins, only: %i[create update destroy]
   resources :stories do
     resources :comments, module: :stories, only: %i[show create]
   end
