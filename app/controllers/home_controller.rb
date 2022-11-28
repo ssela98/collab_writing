2c402f7294436afa702a297bf95f0f3dee212d82
ssela98 <tirnavanalessandra@gmail.com>
2022-11-29 00:48:16 +0100

    store settings in session

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,14 @@
 
 class HomeController < ApplicationController
   def index
-    time_filter = case params[:filter_by_time]
-                  when 'today'
-                    Date.today.at_beginning_of_day
-                  when 'this_week'
-                    Date.today.at_beginning_of_week
-                  when 'this_month'
-                    Date.today.at_beginning_of_month
-                  when 'this_year'
-                    Date.today.at_beginning_of_year
-                  end
-    filtered_stories = Story.where(visible: true).filter_by_time(time_filter)
-    order_by_votes = { cached_weighted_like_score: :desc } if params[:order_by]&.[]('top')
-    order_by_time = { created_at: params[:order_by]&.[]('new') ? :asc : :desc }
-    filtered_and_ordered_stories = filtered_stories.order(order_by_votes).order(order_by_time).with_rich_text_content
+    session[:stories_filter_by] ||= 'all_time' # default filter
+    session[:stories_order_by] ||= 'top' # default order
+
+    session[:stories_filter_by] = params[:filter_by_date] if params[:filter_by_date]
+    session[:stories_order_by] = params[:order_by] if params[:order_by]
+
+    filtered_stories = Story.where(visible: true).filter_by_date_keyword(session[:stories_filter_by])
+    filtered_and_ordered_stories = filtered_stories.order_by_keyword(session[:stories_order_by]).with_rich_text_content
 
     @pagy, @stories = pagy(filtered_and_ordered_stories, items: 50)
   end
