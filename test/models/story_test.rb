37a6d197a42eb0a6de5d2c08fe6f6849a10af729
ssela98 <tirnavanalessandra@gmail.com>
2022-11-28 02:15:53 +0100

    annotate models

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -4,12 +4,19 @@
 #
 # Table name: stories
 #
-#  id         :integer          not null, primary key
-#  user_id    :integer          not null
-#  title      :string           not null
-#  visible    :boolean          default(TRUE)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                             :integer          not null, primary key
+#  user_id                        :integer          not null
+#  title                          :string           not null
+#  visible                        :boolean          default(TRUE)
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#  cached_scoped_like_votes_total :integer          default(0)
+#  cached_scoped_like_votes_score :integer          default(0)
+#  cached_scoped_like_votes_up    :integer          default(0)
+#  cached_scoped_like_votes_down  :integer          default(0)
+#  cached_weighted_like_score     :integer          default(0)
+#  cached_weighted_like_total     :integer          default(0)
+#  cached_weighted_like_average   :float            default(0.0)
 #
 require 'test_helper'
 
