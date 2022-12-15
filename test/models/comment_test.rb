5c4c90b916fe41a3ac7bbe82663fe79f1054c155
ssela98 <tirnavanalessandra@gmail.com>
2022-12-15 23:50:22 +0100

    add comment model destroy cascade test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -158,4 +158,17 @@ class CommentTest < ActiveSupport::TestCase
 
     assert_equal pin, comment.pin
   end
+
+  test 'destroying should destroy pins and nested comments' do
+    comment = create(:comment)
+    nested_comment = create(:comment, parent: comment)
+    nested_comment_2 = create(:comment, parent: nested_comment)
+    pin = create(:pin, comment:)
+
+    assert_difference 'Pin.count', -1 do
+      assert_difference 'Comment.count', -3 do
+        comment.destroy
+      end
+    end
+  end
 end
