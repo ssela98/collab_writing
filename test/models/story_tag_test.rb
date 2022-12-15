25bb23abad5716be58b12a17f7431b01d20ec6a6
ssela98 <tirnavanalessandra@gmail.com>
2022-12-15 22:43:43 +0100

    add story_tag model tests

--- test/models/story_tag_test.rb
+++ test/models/story_tag_test.rb
@@ -13,7 +13,45 @@
 require 'test_helper'
 
 class StoryTagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @story = create(:story)
+    @tag = create(:tag)
+  end
+
+  test 'should create' do
+    story_tag = StoryTag.create(story: @story, tag: @tag)
+
+    assert story_tag.valid?
+  end
+
+  test 'should not create without story and should return validation error' do
+    story_tag = StoryTag.create(tag: @tag)
+
+    assert_not story_tag.valid?
+    assert_equal ['Story must exist'], story_tag.errors.full_messages
+  end
+
+  test 'should not create without tag and should return validation error' do
+    story_tag = StoryTag.create(story: @story)
+
+    assert_not story_tag.valid?
+    assert_equal ['Tag must exist'], story_tag.errors.full_messages
+  end
+
+  test 'destroy should destroy orphan tag' do
+    story_tag = create(:story_tag, story: @story, tag: @tag)
+
+    assert_difference 'Tag.count', -1 do
+      story_tag.destroy
+    end
+  end
+
+  test 'destroy should not destroy non-orphan tag' do
+    create(:story_tag, tag: @tag)
+    story_tag = create(:story_tag, story: @story, tag: @tag)
+
+    assert_difference 'Tag.count', 0 do
+      story_tag.destroy
+    end
+  end
 end
