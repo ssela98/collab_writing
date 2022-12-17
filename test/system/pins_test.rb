33251cd0fb1d59bc7b84f72a669a429d41358ab2
ssela98 <tirnavanalessandra@gmail.com>
2022-12-17 03:16:37 +0100

    rubocop

--- test/system/pins_test.rb
+++ test/system/pins_test.rb
@@ -7,35 +7,35 @@ class PinsTest < ApplicationSystemTestCase
     @pin = create(:pin)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit pins_url
-    assert_selector "h1", text: "Pins"
+    assert_selector 'h1', text: 'Pins'
   end
 
-  test "should create pin" do
+  test 'should create pin' do
     visit pins_url
-    click_on "New pin"
+    click_on 'New pin'
 
-    click_on "Create pin"
+    click_on 'Create pin'
 
-    assert_text "pin was successfully created"
-    click_on "Back"
+    assert_text 'pin was successfully created'
+    click_on 'Back'
   end
 
-  test "should update pin" do
+  test 'should update pin' do
     visit pin_url(@pin)
-    click_on "Edit this pin", match: :first
+    click_on 'Edit this pin', match: :first
 
-    click_on "Update pin"
+    click_on 'Update pin'
 
-    assert_text "pin was successfully updated"
-    click_on "Back"
+    assert_text 'pin was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy pin" do
+  test 'should destroy pin' do
     visit pin_url(@pin)
-    click_on "Destroy this pin", match: :first
+    click_on 'Destroy this pin', match: :first
 
-    assert_text "pin was successfully destroyed"
+    assert_text 'pin was successfully destroyed'
   end
 end
